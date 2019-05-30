a30db22cc066a0fd06fb3601d1e69c34a7f7ee72
Nathan Thomas <nwthomas@me.com>
2019-05-29 22:35:12 -0700

    Add initial file directory and files

---
+++ src/wordCount/Main.java
@@ -0,0 +1,16 @@
+package wordCount;
+
+import java.util.*;
+
+public class Main
+{
+	public static ArrayList workCount()
+	{
+		// Finish
+	}
+
+	public static void main(String[] args)
+	{
+		wordCount();
+	}
+}
