d0e2fa6b1ce0041f3a434afd997f737821b53bd0
Nathan Thomas <nwthomas@me.com>
2019-05-29 23:17:56 -0700

    Create removePunctuation() method inside Main.java class and scrub string

--- src/wordCount/Main.java
+++ src/wordCount/Main.java
@@ -4,10 +4,20 @@ import java.util.*;
 
 public class Main
 {
+	private static String removePunctuation(String phrase)
+	{
+		return phrase.replaceAll("[[\\.\\?\\!\\,\\;\\:\\{\\}\\(\\)\\']]", "");
+	}
+
 	private static void wordCount()
 	{
+		// Create a new instance of the WordCount phrase and assign it to the "phrase" variable
 		WordCount phrase = new WordCount();
-		System.out.println(phrase.getPhrase());
+
+		// Remove punctuation and returns a string assigned to a new variable cleanPhrase
+		String cleanPhrase = removePunctuation(phrase.getPhrase());
+
+		// Create an array of words based off of the cleanPhrase variable
 	}
 
 	public static void main(String[] args)
