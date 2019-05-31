b84df4db57eb4e8a3bb07c45c7b5726702c3c1eb
Nathan Thomas <nwthomas@me.com>
2019-05-30 19:39:12 -0700

    Finish stretch assignment with alpha-ordered words

--- src/wordCount/Main.java
+++ src/wordCount/Main.java
@@ -1,7 +1,7 @@
 package wordCount;
 
-import java.util.*;
-import java.io.*;
+import java.util.*; // Gives me the Collections/sorts
+import java.io.*; // Gives me the .toLowerCase() method
 
 public class Main
 {
@@ -47,13 +47,38 @@ public class Main
 			}
 		});
 
-
 		// Bring the top 50 words to the screen from biggest on down
 		for (int i = 0; i < 50; i++)
 		{
 			System.out.println("The #" + (i + 1) + " most common word was \"" + sortedMap.get(i).getKey() + ".\" It occured " + sortedMap.get(i).getValue() + " times.");
 			System.out.println();
 		}
+
+		// STRETCH GOAL
+		// Method call to sort the top 50 words from the HashMap alphabetically
+		// Create new ArrayList
+		ArrayList<HashMap.Entry<String, Integer>> top50Words = new ArrayList<HashMap.Entry<String, Integer>>();
+		for (int i = 0; i < 50; i++)
+		{
+			top50Words.add(sortedMap.get(i));
+		}
+
+		Collections.sort(top50Words, new Comparator<Map.Entry<String, Integer>>()
+		{
+			public int compare (HashMap.Entry<String, Integer> o1, HashMap.Entry<String, Integer> o2)
+			{
+				return o2.getKey().compareTo(o1.getKey());
+			}
+		});
+
+		System.out.println("The top 50 words ordered alphabetically are:");
+		for (int i = 0; i < 50; i++)
+		{
+			System.out.println(top50Words.get(i).getKey());
+		}
+
+
+		// print only the key values to the screen
 	}
 
 	public static void main(String[] args)
